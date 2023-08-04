f299222baa3b23e07f078b786dbd94b8ff6d3962
scheng39 <scheng39@myseneca.ca>
2023-08-04 15:25:19 -0400

    init

---
+++ js/app.js
@@ -0,0 +1,110 @@
+/*
+ * WEB222 – Assignment
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Please update the following with your information:
+ *
+ *      Name:       SIU MAN CHENG
+ *      Student ID: 121104228
+ *      Date:       10 AUG 2023
+ **/
+
+// All of our data is available on the global `window` object.
+// Create local variables to work with it in this file.
+const { review } = window;
+
+
+//first step: when DOM is loaded
+addEventListener("DOMContentLoaded", function () {
+
+console.log("Loaded!");
+
+//Step2: display review cards dynamically
+
+displayReview();
+
+
+//DOM: when form is submitted
+  const reviewform = document.querySelector(".myForm");
+  reviewform.addEventListener("submit", updateReview);
+
+  function updateReview(e) {
+    
+    e.preventDefault();
+
+    const username = document.querySelector("#review-username").value;
+    const date = document.querySelector("#review-date").value;
+    const comment = document.querySelector("#review-comment").value;
+    const rating = document.querySelector('input[name="rating"]:checked').value;
+
+    const newReview = {name: username, date:date, rating: parseInt(rating), comment: comment};
+    review.push(newReview);
+
+    let reviewWrapBox = document.querySelector(".review-wrapper");
+    reviewWrapBox.innerHTML = "";
+    displayReview();
+  }
+
+
+
+
+
+}); //END of DOM
+
+
+function displayReview(){
+  let reviewWrapBox = document.querySelector(".review-wrapper");
+
+  review.forEach(function (reviews, index){
+
+    const indiReview = document.createElement("div");
+    // Create a <div> to hold the card
+    // Add the .indireview class to the <div>
+    indiReview.classList.add("indireview");
+  
+    // Create a <p> to hold stars
+    const stars = document.createElement("p");
+
+    for (let i = 0; i < 5; i++) {
+      const star = document.createElement("span");
+      let rating = reviews.rating;
+      star.classList.add("material-icons");
+      if (i < rating) {
+        star.classList.add("checked");
+      }
+      star.innerText = "star";
+      stars.appendChild(star);
+    }
+  
+    // Create a <p> to hold user review
+    const commentbox = document.createElement("p");
+    commentbox.classList.add("textreview");
+    commentbox.innerText = reviews.comment;
+  
+  
+    // Create a <p> to hold user name
+    const usernamebox = document.createElement("p");
+    usernamebox.classList.add("userreview");
+    usernamebox.innerText = reviews.name;
+    
+    // Create a <p> to hold date
+    const datebox = document.createElement("p");
+    datebox.classList.add("reviewdate");
+    datebox.innerText = reviews.date;
+  
+    //add to indireview
+    indiReview.appendChild(stars);
+    indiReview.appendChild(commentbox);
+    indiReview.appendChild(usernamebox);
+    indiReview.appendChild(datebox);
+  
+    //add to review wrapper
+    reviewWrapBox.appendChild(indiReview);
+  
+    });
+}
+
---
+++ js/data.js
@@ -0,0 +1,38 @@
+/**
+ * data.js
+ *
+ * The list of review
+ */
+
+window.review = [
+  {
+    name: "CatLover42",
+    date: "2023-07-12",
+    rating: 5,
+    comment: "Exploding Kittens is the purr-fect card game for cat enthusiasts like me! "
+  },
+  {
+    name: "GamerGuru87",
+    date: "2023-07-12",
+    rating: 4,
+    comment: "The quick gameplay is perfect for short sessions or as a warm-up for longer gaming sessions."
+  },
+  {
+    name: "MumatHome",
+    date: "2023-07-12",
+    rating: 4,
+    comment: "Exploding Kittens has become a family favorite! It's a game for everyone!"
+  },
+  {
+    name: "BoardGameLover",
+    date: "2023-07-12",
+    rating: 3,
+    comment: "It's a great game to play with friends who love a challenge and appreciate a bit of randomness to keep things interesting."
+  },
+  {
+    name: "PartyAnimal99",
+    date: "2023-07-12",
+    rating: 5,
+    comment: "The game's humor and fast-paced nature make it a hit with everyone. It's easy to teach new players, so no one feels left out."
+  }
+];
